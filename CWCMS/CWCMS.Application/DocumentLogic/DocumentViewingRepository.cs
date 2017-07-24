8f334574409643702c18d52ce4a565e50db04972
berk.buyukyildiz <berk.buyukyildiz@outlook.com>
2017-07-24 13:40:14 +0300

    Some changes applied on document viewing methods

--- CWCMS/CWCMS.Application/DocumentLogic/DocumentViewingRepository.cs
+++ CWCMS/CWCMS.Application/DocumentLogic/DocumentViewingRepository.cs
@@ -8,26 +8,7 @@ using CWCMS.Core.Models;
 
 namespace CWCMS.Application.DocumentRepositoryLogic
 {
-    class DocumentViewingRepository : IDocumentViewingRepository
+    class DocumentViewingRepository
     {
-        public IEnumerable<Document> ListActiveDocumentsByCategory(int documentTypeID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Document> ListCancelledDocumentsByCategory(int documentTypeID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Document> ListRevisedDocumentsByCategory(int documentTypeID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Document searchDocumentByReferenceCode(string referenceNumber)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
