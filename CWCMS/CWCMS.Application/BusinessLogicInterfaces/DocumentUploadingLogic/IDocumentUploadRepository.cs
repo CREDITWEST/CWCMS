e2321469d7e2666638765433fd5d0c505689b94c
berkbykyldz <berk.buyukyildiz@outlook.com>
2017-07-25 10:47:39 +0300

    New Repos for Uploading Document Created

---
+++ CWCMS/CWCMS.Application/BusinessLogicInterfaces/DocumentUploadingLogic/IDocumentUploadRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CWCMS.Core.Models;
+using CWCMS.Infrastructure.Repositories;
+
+namespace CWCMS.Application.BusinessLogicInterfaces.DocumentUploadingLogic
+{
+    public interface IDocumentUploadRepository
+    {
+        void UploadToServerDocument(Document createdRecord);
+    }
+}
---
+++ CWCMS/CWCMS.Application/DocumentLogic/DocumentUploadingRepositories/DocumentUploadRepository.cs
@@ -0,0 +1,33 @@
+﻿using CWCMS.Application.BusinessLogicInterfaces.DocumentUploadingLogic;
+using CWCMS.Core.Models;
+using CWCMS.Infrastructure.Repositories;
+
+namespace CWCMS.Application.DocumentLogic.DocumentUploadingRepositories
+{
+    internal class DocumentUploadRepository : IDocumentUploadRepository
+    {
+        private DocumentRepository _documentRepository;
+
+        /* Normally this part contains much more detail
+         * but for getting ready to 07/28 Demonstration
+         * includes only simple crud op content
+         * In normal circumstances this class should do
+         * getting Created Doc model adding reference and guid
+         * checking is this reference is in SQL
+         * Filling necessary part and saving its GUID to the wait signature table
+        */
+
+        private Document _document;
+
+        public DocumentUploadRepository()
+        {
+            this._documentRepository = new DocumentRepository();
+            this._document = new Document();
+        }
+
+        public void UploadToServerDocument(Document createdRecord)
+        {
+            _documentRepository.Add(createdRecord);
+        }
+    }
+}
