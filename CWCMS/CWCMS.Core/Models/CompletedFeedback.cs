8562c0a93e1fc2f32a7720250c34e431428c520c
berkbykyldz <berk.buyukyildiz@outlook.com>
2017-07-25 10:05:15 +0300

    Models updated wrt database updates

--- CWCMS/CWCMS.Core/Models/CompletedFeedback.cs
+++ CWCMS/CWCMS.Core/Models/CompletedFeedback.cs
@@ -3,7 +3,7 @@
     public class CompletedFeedback
     {
         // The Auto-Increment integer type variable which is primary key for the entity
-        public int CompletedFeedID { get; set; }
+        public int CompFeedID { get; set; }
 
         // The id of the feedback record that has responded to the its requester
         public int FeedbackID { get; set; }
