36d6529383d7629aea796eede1929fb85ad743ec
Ecem <ecemgo@hotmail.com>
2017-07-27 12:53:31 +0300

    documentResource dosyası güncellendi.

--- CWCMS/CWCMS.UI/Scripts/app/document/documentResource.js
+++ CWCMS/CWCMS.UI/Scripts/app/document/documentResource.js
@@ -1,11 +1,11 @@
-﻿	angular.module('resDocuman').factory("DocumansResource", [
-        "$resource", function ($resource) {
-            "use strict";
-            return $resource(+ "/api/Document/:id", { id: "@id" }, {
-                get: { method: "GET", url: "/api/Document/" },
-                sendtypeid: { method: "POST", url: "/api/Document/sendtypeid" },
-         
+﻿angular.module('resDocuman').factory("DocumansResource", [
+    "$resource", function ($resource) {
+        "use strict";
+        return $resource(+ "/api/Document/:id", { id: "@id" }, {
+            get: { method: "GET", url: "/api/Document/" },
+            sendtypeid: { method: "POST", url: "/api/Document/sendtypeid" },
+
         });
     }
 ]);
-  
+
